17bd24db96319fd27893c0c29a50445e8c427077
JuubiSnake <wellm018@gmail.com>
2015-10-23 14:09:13 +0100

    call
    egtergergreth

--- task_2/Findmii/app/src/main/java/com/g3/findmii/MapsActivity.java
+++ task_2/Findmii/app/src/main/java/com/g3/findmii/MapsActivity.java
@@ -42,7 +42,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         progressDialog = new ProgressDialog(this);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.//somethingelse
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
