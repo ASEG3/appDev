eece5944c5db2cb501917b13ad1497bdb01572ef
JuubiSnake <wellm018@gmail.com>
2015-10-23 14:12:24 +0100

    Task 2 and 3 now in dev branch
    The sickest task

---
+++ task_3/Findmii/app/src/main/java/com/g3/findmii/MainActivity.java
@@ -0,0 +1,109 @@
+package com.g3.findmii;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    double longlitude;
+    double latitude;
+    LocationManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+
+        Button nextButton = (Button) findViewById(R.id.location_button);
+        setButtonListener(nextButton);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public MainActivity getActivity() {
+        return this;
+    }
+
+    public void setButtonListener(Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("PRESSED","IMPRESSEd");
+                try {
+                    if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setMessage("GPS is not enabled, please enable it")
+                                .setPositiveButton("Settings...", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                        startActivity(i);
+                                    }
+                                })
+                                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // User cancelled the dialog
+                                    }
+                                });
+                        builder.create();
+                        builder.show();
+                    } else {
+                        Intent i = new Intent(getApplicationContext(),MapsActivity.class);
+                        startActivity(i);
+                    }
+                }
+                catch (SecurityException e) {
+                    Log.w(e.getMessage(),"GETITNOW");
+                }
+            }
+        });
+    }
+
+    public void setLongAndLat(Location location){
+        this.longlitude = location.getLongitude();
+        this.latitude = location.getLatitude();
+    }
+}
